e59ca573be9912553317e667a3dd1cd04a73ffa8
Flavia-20 <flaviamonique38@gmail.com>
2024-03-29 23:38:24 -0300

    Aula 2
    código referente a aula 2 do curso

---
+++ src/Conta.java
@@ -0,0 +1,9 @@
+public class Conta {
+
+    /* essa classe Conta tem quatro atributos*/
+   double saldo;
+   int agencia;
+   int numero;
+   String titular;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+    /*objeto ou instancia do tipo conta*/
+        Conta primeiraConta = new Conta();
 
-        System.out.println("Hello world!");
+        primeiraConta.saldo = 200;/*coloca 200 nno atributo que se chama saldo*/
+        System.out.println(primeiraConta.saldo);
+        primeiraConta.saldo +=100;
+        System.out.println(primeiraConta.saldo);
 
+        Conta segundaConta = new Conta();
+        segundaConta.saldo=50;
+
+        System.out.println("primeira conta tem: "+ primeiraConta.saldo);
+        System.out.println("Segunda conta tem: " + segundaConta.saldo);
+
+        if(primeiraConta == segundaConta){
+            System.out.println("As conmtas são a mesma");
+        }else{
+            System.out.println("As contas são diferentes");
+        }
     }
 }
